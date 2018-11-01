306c0ee3fadcb5657c8f276f4fbcdb9f9d5f6803
Zsprezzatura <zslaf369@163.com>
2018-11-01 14:45:09 +0800

    签到系统修改

--- Navigation/src/js/main.js
+++ Navigation/src/js/main.js
@@ -16,28 +16,28 @@ $(function(){
         });
     });
 
-    function Choices(){
-        $(".Choices-banner ul").animate({marginLeft:"-1184px"},2000, function () {  
-                $(".Choices-banner ul>li").eq(0).appendTo($(".Choices-banner ul"));  
-                $(".Choices-banner ul").css('marginLeft','0px');  
-        });
+    // function Choices(){
+    //     $(".Choices-banner ul").animate({marginLeft:"-1184px"},2000, function () {  
+    //             $(".Choices-banner ul>li").eq(0).appendTo($(".Choices-banner ul"));  
+    //             $(".Choices-banner ul").css('marginLeft','0px');  
+    //     });
           
-        $(".Choices-tel ul").css('marginLeft','-1184px');  
-        $(".Choices-tel ul>li").eq(0).appendTo($(".Choices-tel ul"));  
-        $(".Choices-tel ul").css({marginLeft:"0px"},2000);
-    }
-    var tabChange = setInterval(Choices,5000);//每隔3000毫秒（3秒）调用一次Choices函数
+    //     $(".Choices-tel ul").css('marginLeft','-1184px');  
+    //     $(".Choices-tel ul>li").eq(0).appendTo($(".Choices-tel ul"));  
+    //     $(".Choices-tel ul").css({marginLeft:"0px"},2000);
+    // }
+    // var tabChange = setInterval(Choices,5000);//每隔3000毫秒（3秒）调用一次Choices函数
 
-    $('.Choices-banner').mouseover(function(){
-        clearInterval(tabChange);//鼠标悬停在Choices-banner上时,清除定时器，轮播停止
-    });
+    // $('.Choices-banner').mouseover(function(){
+    //     clearInterval(tabChange);//鼠标悬停在Choices-banner上时,清除定时器，轮播停止
+    // });
 
-    $('.Choices-banner').mouseout(function(){
-        tabChange = setInterval(Choices,5000);
-    });
+    // $('.Choices-banner').mouseout(function(){
+    //     tabChange = setInterval(Choices,5000);
+    // });
 
-    $('.Choicesnext').click(function () {
-        Choices();
-    })
+    // $('.Choicesnext').click(function () {
+    //     Choices();
+    // })
 
 })
--- signin/js/main.js
+++ signin/js/main.js
@@ -14,7 +14,7 @@ $('#Tsubmit').on('click',function getTsubmit() {
             'position': $("#position001").val(),
             'phone': $("#phone001").val(),
             'email': $("#email001").val(),
-            'activity': '人力资源研讨会',
+            'activity': '国际学校发展大会',
         }, 
         url: changeUrl.address + '/manager/checkIn/add.do', 
         success: function(res) {
@@ -42,50 +42,7 @@ layui.use('form', function(){
 });
 
 
-layui.use('table', function(){
-  var table = layui.table;
-  table.on('tool(user)', function(obj){
-    var data = obj.data;
-      if(obj.event === 'del'){
-        layer.confirm('确认删除此项', function(index){
-          obj.del();
-          layer.close(index);
-        });
-      }
-      // else if(obj.event === 'detail'){
-      else if(obj.event === 'lookQR'){           
-            $.ajax({
-              type : "POST",
-              async: true,
-              traditional: true,
-              data: {//提交的参数
-                  'imageUrl': 'http://qr.liantu.com/api.php?text='+data.token+'',       
-              }, 
-              url: changeUrl.address + '/activity/get_ticket_image.do', 
-              success :   function(msg) {
-                  // alert('成功')
-                  console.log(msg.data);
-                  var imgUrl=msg.data
-                  console.log("ajax:"+imgUrl);
-                  //ES6写法 layer.alert(`<span>${data.name}</span><br><img class="qrcode" src="${imgUrl}"/>`);
-                  layer.alert('<span class="Qrtitle">'+data.name+'</span>' + '<img class="qrcode" src="'+imgUrl+'"/>');
-              },
-              error: function(err) {
-                  alert('发生错误，请求数据失败！');
-              }
-            });
 
-
-        // console.log(imgUrl);
-        
-
-      } 
-      else if(obj.event === 'edit'){
-        layer.alert('编辑行：<br>'+ JSON.stringify(data))
-        $('#test').text(data.id)
-      }
-  });
-});  
 
 
 //新增参会信息弹窗
@@ -109,22 +66,22 @@ $('#J-add').on('click',function () {
 
 layui.use(['table','layer'], function(){
   var table = layui.table;
-  
   //方法级渲染
   table.render({
     elem: '#LAY_table_user'
     ,url: changeUrl.address+'/manager/checkIn/list.do'
-    ,cellMinWidth: 80 //全局定义常规单元格的最小宽度
     ,limit:20
+    ,page: true
+    // ,cellMinWidth: 80 //全局定义常规单元格的最小宽度
     // ,initSort: {
     //   field: 'id' //排序字段，对应 cols 设定的各字段名
     //   ,type: 'asc' //排序方式  asc: 升序、desc: 降序、null: 默认排序
-    // }
+    // }   
     ,request:{
       pageName:'pageNum',
       limitName:'pageSize'
     }
-    ,height: 'full-200'
+    ,height: 'full-90'
     ,cols: [[//表头
       {field:'id', title: '编号', fixed: true, align:'center'}
       ,{field:'name', title: '姓名', width:120, align:'center'}
@@ -134,18 +91,17 @@ layui.use(['table','layer'], function(){
       ,{field:'phone', title: '电话', width:140, align:'center'}
       ,{field:'email', title: '邮箱', width:200, align:'center'}
       ,{toolbar: '#imgQR', title: '二维码', event:'lookQR', width:120, align:'center'}
-      // ,{field:'token', title: '二维码', event:'lookQR'}
       ,{field:'activity', title: '活动明细', width:160, align:'center'}
-      ,{field:'createTime', title: '提交时间', width:180, align:'center'}
-      ,{field:'checkinTime', title: '签到时间', width:170, fixed:'right',align:'center'}
+      ,{field:'createTime', title: '提交时间', width:180, templet: '<div>{{ layui.laytpl.toDateString(d.createTime) }}</div>',align:'center'}
+            // ,{field:'checkinTime', title: '签到时间', width:170, fixed:'right', align:'center'}
+      ,{field:'checkinTime', title: '签到时间', width:170, fixed:'right', templet: '<div>{{ layui.laytpl.toDateString(d.checkinTime) }}</div>',align:'center'}
       // ,{toolbar: '#barDemo', title: '操作',width:180, align:'center',fixed:'right'} 
     ]]
     ,id: 'testReload'
-    ,page: true
-    ,height: 600
   });
   var $ = layui.$, active = {
     reload: function(){
+      //获取input的值
       var demoReload = $('#demoReload').val();
       //执行重载
       table.reload('testReload', {
@@ -153,8 +109,9 @@ layui.use(['table','layer'], function(){
           curr: 1 //重新从第 1 页开始
         }
         ,where: { //给接口传的字段
-           pageSize:1,
-           pageNum:1,
+           // pageSize:1,
+           // pageNum:1,
+           type: 0,
            keyword: demoReload
          }
       });
@@ -165,6 +122,90 @@ layui.use(['table','layer'], function(){
     var type = $(this).data('type');
     active[type] ? active[type].call(this) : '';
   });
+
+
+  //时间戳处理
+  layui.laytpl.toDateString = function(d, format){
+        // var date = new Date(d || new Date())
+        var date = new Date(d)
+        ,ymd = [
+        this.digit(date.getFullYear(), 4)
+        ,this.digit(date.getMonth() + 1)
+        ,this.digit(date.getDate())
+        ]
+        ,hms = [
+        this.digit(date.getHours())
+        ,this.digit(date.getMinutes())
+        ,this.digit(date.getSeconds())
+        ];
+        if (d === null) {
+          format = '未签到';
+          return format.replace();
+        }else{   
+          // format = format || 'yyyy-MM-dd HH:mm:ss';
+          format = 'yyyy-MM-dd HH:mm:ss';   
+          return format.replace(/yyyy/g, ymd[0])
+          .replace(/MM/g, ymd[1])
+          .replace(/dd/g, ymd[2])
+          .replace(/HH/g, hms[0])
+          .replace(/mm/g, hms[1])
+          .replace(/ss/g, hms[2]);
+        }
+  };
+  //数字前置补零
+  layui.laytpl.digit = function(num, length, end){
+        var str = '';
+        num = String(num);
+        length = length || 2;
+        for(var i = num.length; i < length; i++){
+        str += '0';
+        }
+        return num < Math.pow(10, length) ? str + (num|0) : num;
+  };  
+
+
 });
+layui.use('table', function(){
+  var table = layui.table;
+  table.on('tool(user)', function(obj){
+    var data = obj.data;
+      if(obj.event === 'del'){
+        layer.confirm('确认删除此项', function(index){
+          obj.del();
+          layer.close(index);
+        });
+      }
+      // else if(obj.event === 'detail'){
+      else if(obj.event === 'lookQR'){           
+            $.ajax({
+              type : "POST",
+              async: true,
+              traditional: true,
+              data: {//提交的参数
+                  'imageUrl': 'http://qr.liantu.com/api.php?text='+data.token+'',       
+              },
+              // url: 'http://data.xinxueshuo.cn:80/nsi-1.0/activity/get_ticket_image.do',
+              url: changeUrl.address + '/activity/get_ticket_image.do', 
+              success :   function(msg) {
+                  // alert('成功')
+                  // console.log(msg.data);
+                  var imgUrl=msg.data
+                  // console.log("ajax:"+imgUrl);
+                  //ES6写法 layer.alert(`<span>${data.name}</span><br><img class="qrcode" src="${imgUrl}"/>`);
+                  layer.alert('<span class="Qrtitle">'+data.name+'</span>' + '<img class="qrcode" src="'+imgUrl+'"/>');
+              },
+              error: function(err) {
+                  alert('发生错误，请求数据失败！');
+              }
+            });
+        // console.log(imgUrl);
+      } 
+      else if(obj.event === 'edit'){
+        layer.alert('编辑行：<br>'+ JSON.stringify(data))
+        $('#test').text(data.id)
+      }
+  });
+});  
+
 })
 
